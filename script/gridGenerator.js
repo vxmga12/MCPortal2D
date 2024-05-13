fc63226ae86e87faef96280612eabc9f611d80a5
Vasco Augusto <vxmga12@gmail.com>
2024-05-13 17:56:01 +0100

    Added proper background scene

--- script/gridGenerator.js
+++ script/gridGenerator.js
@@ -3,6 +3,15 @@ const wrapper = document.getElementById("wrapper");
 for (let i = 1; i <= 2048; i++) {
     const div = document.createElement('div');
     div.classList.add("cellBlock");
+    if(i <= 1024){
+        div.classList.add("skyBlock");
+    }
+    else if (i > 1024 && i <= 1088) {
+        div.classList.add("grassBlock");
+    }
+    else if ( i > 1088 && i <= 2048){
+        div.classList.add("dirtBlock");
+    }
     //div.innerHTML = i;
 
     wrapper.appendChild(div);
